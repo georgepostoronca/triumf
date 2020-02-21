b47282b33e80d9b8f0233e26f8dd4140d2800bfa
georgepostoronca <george.postoornca.work@gmail.com>
2020-02-22 01:47:55 +0300

    block form

--- js/main-babel.js
+++ js/main-babel.js
@@ -1,3 +1,36 @@
 import $ from 'jquery';
+import "jquery-validation";
+import imask from "imask";
+console.log($);
 
-alert($);
+// Mask Phone
+var element = document.querySelector('input[type="phone"');
+var maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+var mask = IMask(element, maskOptions);
+
+
+// Form Vlidation
+$.validator.setDefaults({
+	debug: false,
+	success: 'valid',
+	rules: {
+		email: {
+			required: true,
+			email: true,
+		}
+	},
+	errorPlacement: function(error, element) {
+		$(this).removeClass('valid');
+		$(this).addClass('error');
+	},
+	success: function(label, element) {
+		$(this).removeClass('error');
+		$(this).addClass('valid');
+	},
+});
+
+$(".form-validation").each(function() {
+    $(this).validate();
+});
